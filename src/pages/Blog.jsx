61c6ced8173a03f9a067025e98f17184f476d4a3
mckennamm <molly.mckenna88@gmail.com>
2025-03-25 15:06:29 -0400

    adding iframe for blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,9 +4,11 @@ import './Blog.css'
 const Blog = () => {
     return (
         <div className="blog">
-            <h1 className="blog-title">Hi this is blog</h1>
-            <p className="blog-description">This is a blog page</p>
-            <iframe src ="https://studiomolly.blogspot.com/" width="100%" height="500"> </iframe>
+            <h1 className="blog-title">Check out my blog!</h1>
+            <p className="blog-description">Welcome to Studio Molly—a space where creativity, curiosity, and code collide. This blog is a reflection of my journey as a web designer, blending the technical with the artistic to craft digital experiences that feel meaningful and intentional. Here, I share stories from my path—insights into my creative process, lessons learned from building personal and freelance projects, and reflections on the ever-evolving world of web design. <br/><br/>
+
+But this space is more than just a chronicle of my work. It’s a celebration of growth, curiosity, and the beauty found in both success and struggle. Whether I’m exploring new design concepts, reflecting on my artistic influences, or navigating the nuances of carving out a career in tech, my goal is to connect, inspire, and offer a glimpse into the world I’m building—one line of code and one creative spark at a time.<br/><br/></p>
+            <iframe src ="https://studiomolly.blogspot.com/" width="70%" height="1000" className="blog-iframe"> </iframe>
 
 
         </div>
